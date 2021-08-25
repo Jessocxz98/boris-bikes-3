e3cebae9c40a53fc4fd4c0a5915ae04f15bde961
Chloe <chloeemstokes@gmail.com>
2021-08-25 17:56:29 +0100

    Addition of dock method and instance variables so bike can be docked within programme.

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,10 +2,24 @@ require "DockingStation"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  
+
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+  
 end
 
