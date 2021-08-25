28e2f9e78c389548a4708fb5eff58778c7f3ab62
Chloe <chloeemstokes@gmail.com>
2021-08-25 15:02:00 +0100

    Added the lib file and linked up the files

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,3 +1,7 @@
-describe DockingStation do
+require "DockingStation"
 
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
 end
+
+# expect(subject).to respond_to :release_bike
